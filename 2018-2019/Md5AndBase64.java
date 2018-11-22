ff2e63f8611b63f272dd8daed72174e1507bb705
JFMACS <31764113+JFMACS@users.noreply.github.com>
2018-11-22 11:32:06 +0800

    添加了UUID的生成

--- 2018-2019/Md5AndBase64.java
+++ 2018-2019/Md5AndBase64.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.MessageDigest;
 import sun.misc.BASE64Encoder;
+import java.util.UUID;
 
 public class main {
 	public static void main(String[] args) {
@@ -13,6 +14,7 @@ public class main {
 			String code = br.readLine();
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
 			BASE64Encoder base64en = new BASE64Encoder();
+			System.out.println("生成的id：" + UUID.randomUUID().toString().replace("-", ""));
 			System.out.println("加密后的密文：" + base64en.encode(md5.digest(code.getBytes("utf-8"))));
 		} catch (Exception e) {
 			e.printStackTrace();
